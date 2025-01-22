b9fd084cf1a1c3558c10f0d33f68733b984ae581
Val200ya <australia636@gmail.com>
2025-01-22 10:24:23 +0300

    added connection to database

---
+++ Model/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManagerRodina.Model
+{
+    public class Database
+    {
+        private SqlConnection sqlConnection = new SqlConnection(
+            "@Data Source=510EC4;Initial Catalog=MoneyManagerDB;Integrated Security=True");
+
+        public void OpenConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
